4a5b07b04b3cbed25232fc556ff931e7b1c27f1d
Thomas Payton <contact@th-mas.dev>
2021-08-11 09:57:53 -0500

    Added POST support

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+var multer = require('multer')
+var upload = multer()
+
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+app.use(upload.array()); 
+app.use(express.static('public'));
 
 app.post('/api/submit', (req, res) => {
     res.json({
         headers: req.headers,
-        ip: req.ip
+        ip: req.ip,
+        body: req.body
     })
 })
 
